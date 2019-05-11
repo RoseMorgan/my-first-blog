d653abda2b546ab406ea6edd135391a4276b514b
Rose <rose.a.morgan@gmail.com>
2019-05-11 10:52:29 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,21 @@
+print('Hello, Django Girls!')
+my_dictionary = {'name': 'Rose','City':'Brighton'}
+print(my_dictionary)
+if 2 < 1:
+	print('It works!')
+else:
+	print('It still works!')
+name = 'Rose'
+if name == 'Rose':
+	print('Hi Rose!')
+elif name == 'Morgan':
+	print('Sup Morgan?')
+else:
+	print('New phone, who dis?')
+
+buds = ['Alice', 'Charlie', 'Allison']
+def hi():
+	print('Hi!')
+	print('How\'s it going?')
+
+hi("Rose")
